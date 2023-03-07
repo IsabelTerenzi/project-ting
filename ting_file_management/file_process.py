ed91dd0d086a32b2b5b08984f3a94d44ec3e4e58
Isabel Terenzi <bel.terenzi@gmail.com>
2023-03-07 20:45:06 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,7 +1,20 @@
+from ting_file_management import txt_importer
+import sys
+
 
 # Requisito 3 - Implemente a função process no módulo file_process
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for valor in range(len(instance)):
+        if instance.search(valor)["nome_do_arquivo"] == path_file:
+            return None
+    file = txt_importer(path_file)
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": (file),
+    }
+    instance.enqueue(result)
+    return sys.stdout.write(str(result))
 
 
 def remove(instance):
