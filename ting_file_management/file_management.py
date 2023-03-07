bd469a5261ae5ed0e29e0571330e784621299761
Isabel Terenzi <bel.terenzi@gmail.com>
2023-03-07 20:31:42 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+# Requisito 2 - Implemente uma função txt_importer dentro do módulo file_management
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r') as arquivo:
+            result = arquivo.read().split('\n')
+            return result
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
