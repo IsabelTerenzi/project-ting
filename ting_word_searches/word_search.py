e10d0a7652aceabd2c896c11e8e321780d514b7c
Isabel Terenzi <bel.terenzi@gmail.com>
2023-03-07 21:41:03 -0300

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,5 +21,26 @@ def exists_word(word, instance):
     return result
 
 
+# Requisito 8 - Implemente uma função search_by_word
+# que busque uma palavra em todos os arquivos processados
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    qtd_vezes = []
+    result = []
+    for valor in range(len(instance)):
+        arquivo = instance.search(valor)
+        linhas = arquivo['linhas_do_arquivo']
+        for valor in range(len(linhas)):
+            if word.lower() in linhas[valor].lower():
+                qtd_vezes.append({
+                    'linha': valor + 1,
+                    'conteudo': linhas[valor]
+                })
+
+        if len(qtd_vezes) >= 1:
+            result.append({
+                'palavra': word,
+                'arquivo': arquivo['nome_do_arquivo'],
+                'ocorrencias': qtd_vezes
+            })
+
+    return result
